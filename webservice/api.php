5cfd5472a77cfafaa7de238cde08ef8b64eec284
prabaHridayami <37198788+prabaHridayami@users.noreply.github.com>
2019-12-09 10:14:38 +0800

    webservice

---
+++ webservice/api.php
@@ -0,0 +1,95 @@
+<?php 
+	require_once 'connect.php';	
+	$response = array();	
+	if(isset($_GET['apicall'])){	
+		switch($_GET['apicall']){			
+			case 'signup':
+				if(isTheseParametersAvailable(array('name','username','email','password','phone'))){
+                    $name = $_POST['name']; 
+					$username = $_POST['username']; 
+					$email = $_POST['email']; 
+					$password = md5($_POST['password']);
+                    $phone = $_POST['phone'];                                 
+					
+					$stmt = $conn->prepare("SELECT * FROM tb_user WHERE username = ? OR email = ?");
+					$stmt->bind_param("ss", $username, $email);
+					$stmt->execute();
+					$stmt->store_result();
+					
+					if($stmt->num_rows > 0){
+						$response['error'] = true;
+						$response['message'] = 'User already registered';
+						$stmt->close();
+					}else{
+						$stmt = $conn->prepare("INSERT INTO tb_user (`name`, username, email, `password`, phone, created_at, updated_at) VALUES (?, ?, ?, ?, ?, NOW(), NOW())");
+						$stmt->bind_param("sssss", $name, $username, $email, $password, $phone);
+						if($stmt->execute()){
+							$stmt = $conn->prepare("SELECT `name`,username, email, phone FROM tb_user WHERE username = ?"); 
+							$stmt->bind_param("s",$username);
+							$stmt->execute();
+							$stmt->bind_result($name, $username ,$email, $phone);
+							$stmt->fetch();							
+							$user = array(                                
+								'name'=>$name, 								
+								'username'=>$username, 
+								'email'=>$email,
+								'phone'=>$phone
+							);							
+							$stmt->close();							
+							$response['error'] = false; 
+							$response['message'] = 'User registered successfully'; 
+							$response['user'] = $user; 
+						}
+					}					
+				}else{
+					$response['error'] = true; 
+					$response['message'] = 'required parameters are not available'; 
+				}
+				
+			break; 
+			
+			case 'login':				
+				if(isTheseParametersAvailable(array('username', 'password'))){					
+					$username = $_POST['username'];
+					$password = md5($_POST['password']); 					
+					$stmt = $conn->prepare("SELECT id,`name`,username,email,phone FROM tb_user WHERE username = ? AND `password` = ?");
+					$stmt->bind_param("ss",$username, $password);					
+					$stmt->execute();					
+					$stmt->store_result();					
+					if($stmt->num_rows > 0){						
+						$stmt->bind_result($id,$name, $username, $email, $phone);
+						$stmt->fetch();						
+						$user = array(
+							'id'=>$id,                            
+                            'name'=>$name, 
+							'username'=>$username, 
+							'email'=>$email,
+							'phone'=>$phone
+						);						
+						$response['error'] = false; 
+						$response['message'] = 'Login successfull'; 
+						$response['user'] = $user; 
+					}else{
+						$response['error'] = false; 
+						$response['message'] = 'Invalid username or password';
+					}
+				}
+			break; 			
+			default: 
+				$response['error'] = true; 
+				$response['message'] = 'Invalid Operation Called';
+		}		
+	}else{
+		$response['error'] = true; 
+		$response['message'] = 'Invalid API Call';
+	}
+	
+	echo json_encode($response);	
+	function isTheseParametersAvailable($params){		
+		foreach($params as $param){
+			if(!isset($_POST[$param])){
+				return false; 
+			}
+		}
+		return true; 
+	}
